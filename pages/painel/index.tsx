a21625c1c9d5538634d50d4afe5b8e88df7d6592
Guilherme Matheus Corrêa <guilherme.matheus.correa@gmail.com>
2021-01-28 22:37:14 -0300

    Hide empresas from autonomo profile

--- pages/painel/index.tsx
+++ pages/painel/index.tsx
@@ -1,9 +1,50 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { api } from '../../hooks/fetch'
 import Container from '../../components/Container'
 import { useAuth } from '../../hooks/auth'
 
+interface Person {
+  id: number
+  user_id: string
+  cpf: string
+  rg: string
+  rg_exp: string
+  rg_uf: string
+  oab: string
+  oab_uf: string
+  gender: string
+  created_at: string
+  updated_at: string
+  profile_type: string
+  profile_name: string
+  profile_link: string
+  birth_date: null
+  curriculum: string
+  schoolarity: string
+  has_certificate: boolean
+  register_finish: boolean
+}
+
 const PainelCorrespondente: React.FC = () => {
   const { user } = useAuth()
+  const [person, setPerson] = useState<Person>({} as Person)
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const response = await api.get(`/comercial/people/user/${user?.id}`)
+
+        if (response.status === 200) {
+          setPerson(response.data)
+        }
+      } catch (error) {
+        //
+      }
+    }
+
+    loadData()
+  }, [user?.id])
 
   return (
     <div className="container px-5 py-24 mx-auto">
@@ -32,14 +73,19 @@ const PainelCorrespondente: React.FC = () => {
             </p>
           </a>
         </Link>
-        <Link href="/painel/empresas">
-          <a className="bg-white hover:bg-gray-100 shadow-md text-gray-800 text-xl font-semibold trackin-wide px-3 py-12 h-full flex justify-center items-center flex-col rounded-md w-full cursor-pointer">
-            <h3>Empresas</h3>
-            <p className="text-base tracking-wide font-normal text-gray-500">
-              Empresas que você participa.
-            </p>
-          </a>
-        </Link>
+        {user.type === 'E' ||
+          (person?.profile_type ===
+            'Faço parte de um escritório de advocacia' && (
+            <Link href="/painel/empresas">
+              <a className="bg-white hover:bg-gray-100 shadow-md text-gray-800 text-xl font-semibold trackin-wide px-3 py-12 h-full flex justify-center items-center flex-col rounded-md w-full cursor-pointer">
+                <h3>Empresas</h3>
+                <p className="text-base tracking-wide font-normal text-gray-500">
+                  Empresas que você participa.
+                </p>
+              </a>
+            </Link>
+          ))}
+
         <Link href="/painel/contabilidade">
           <a className="bg-white hover:bg-gray-100 shadow-md text-gray-800 text-xl font-semibold trackin-wide px-3 py-12 h-full flex justify-center items-center flex-col rounded-md w-full cursor-pointer">
             <h3>Contabilidade</h3>
