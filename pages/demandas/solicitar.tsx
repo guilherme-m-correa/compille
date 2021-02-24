0a07d094dc5a4b7f9368cd78c46ff18710cba402
Guilherme Matheus Corrêa <guilherme.matheus.correa@gmail.com>
2021-02-24 20:25:16 -0300

    .

--- pages/demandas/solicitar.tsx
+++ pages/demandas/solicitar.tsx
@@ -337,7 +337,7 @@ export default function CadastroAdvogadosCorrespondentesJuridicos() {
                 <Form className="mt-8 space-y-8">
                   <div>
                     {audience?.lawyer && (
-                      <div>
+                      <div className="flex flex-col space-y-2">
                         <span className="text-gray-500 font-medium text-sm">
                           Enviar solicitação para:
                         </span>
@@ -376,7 +376,9 @@ export default function CadastroAdvogadosCorrespondentesJuridicos() {
                           </div>
                         </div>
                         <Link href="/profissionais">
-                          <a>Alterar</a>
+                          <a className="text-sm underline text-gray-500 hover:text-gray-700 cursor-pointer">
+                            Alterar profissional
+                          </a>
                         </Link>
                       </div>
                     )}
